ae69135c422d19910b71c3df2126ed77e873b10f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-01-27 15:29:03 +0900

    to Kyomu

--- nostr-kyomu-haskell/app/Main.hs
+++ nostr-kyomu-haskell/app/Main.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
 import Data.Function
+import Data.Foldable
+import Data.Vector qualified as V
 import Data.Text qualified as T
+import Data.Aeson
+import Data.Aeson.KeyMap qualified as KM
 import Network.WebSockets
 
 main :: IO ()
@@ -18,7 +24,23 @@ main = do
 		fix \a -> do
 			r <- receive conn
 			case r of
-				dm@(DataMessage _ _ _ _) -> send conn dm >> a
+				(DataMessage _ _ _ (Text jsn _)) -> do
+					print @(Maybe Value) $ decode jsn
+					let	mv = recToSend =<< decode jsn
+					case mv of
+						Nothing -> pure ()
+						Just v -> do
+							print v
+							sendDataMessage conn $
+								Text (encode v) Nothing
+					a
 				ControlMessage (Close _ _) -> putStrLn "CLOSE"
 				_ -> print r >> a
 		sendClose conn ("Good-bye!" :: T.Text)
+
+recToSend :: Value -> Maybe Value
+recToSend (Array (toList -> (String "EVENT" : Object ((KM.lookup "id") -> Just (String nm)) : _))) =
+	Just . Array $ V.fromList [String "OK", String nm, Bool True, String ""]
+recToSend (Array (toList -> (String "REQ" : String nm : _))) =
+	Just . Array $ V.fromList [String "EOSE", String nm]
+recToSend _ = Nothing
